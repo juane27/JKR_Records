89277453c25dd0e0e3d2c1a5d423531e10897886
Juan <juanelabayen@gmail.com>
2024-04-28 13:02:23 -0300

    3001

--- backend/JKR_Records/JKR_Records/settings.py
+++ backend/JKR_Records/JKR_Records/settings.py
@@ -140,9 +140,9 @@ ALLOWED_HOSTS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000", 
-    "http://localhost:3000",
-    "http://frontend:3000"
+    "http://127.0.0.1:3001", 
+    "http://localhost:3001",
+    "http://frontend:3001"
 ]
 CORS_ALLOW_CREDENTIALS = False
 
